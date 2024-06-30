769d314fe86c3d10e751fe6e50510910191c102a
Simon Lalonde <simlalonde@hotmail.com>
2024-06-30 17:56:18 -0400

    Add type convert for guess and match expression

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,16 +1,30 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
+    // Type infered from the match expression
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+    println!("Answer is {}", secret_num);
+    
+    // Get the guess as a mutable string
     println!("Guess the number!");
-
     println!("Please input your guess.");
 
     let mut guess = String::new();
-
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
+    
+    // Convert to u32 with shadowing
+    let guess: u32 = guess.trim().parse().expect("Please type an interger!");
     println!("You guessed: {}", guess);
+
+    // Get the result
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!")
+    }
 
 }
